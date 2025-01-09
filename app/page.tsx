a9920d891cbd796b765d3c67ea678cdda863d5b5
fedorchaklm <fedorchak.luba@gmail.com>
2025-01-09 23:38:48 +0200

    a11y, validate for create

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,6 @@ type Props = {
 };
 
 export default function Page(props: Props) {
-  console.log('>', { props })
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
